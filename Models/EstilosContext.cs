f69af883649e622b933f52f3eef36ef95df4c81a
TomasNM229 <67438238+TomasNM229@users.noreply.github.com>
2022-11-06 20:46:24 -0500

    Características no genéricas para el Modelo Publico

--- Models/EstilosContext.cs
+++ Models/EstilosContext.cs
@@ -26,6 +26,18 @@ namespace views_project.Models
                 entidad.Property(e => e.Descripcion).IsRequired().HasMaxLength(200).IsUnicode(false); 
             }
             );
+            modelBuilder.Entity<Publico>(entidad =>{
+                entidad.ToTable("Publico");
+
+                entidad.HasKey(e => e.IDPublico);
+
+                entidad.Property(e => e.Nombre).IsRequired().HasMaxLength(50).IsUnicode(false); 
+                entidad.Property(e => e.Apellido).IsRequired().HasMaxLength(50).IsUnicode(false);
+                entidad.Property(e => e.Edad).IsRequired().HasMaxLength(10).IsUnicode(false);
+                entidad.Property(e => e.Correo).IsRequired().HasMaxLength(100).IsUnicode(false);
+                entidad.Property(e => e.Telefono).IsRequired().HasMaxLength(20).IsUnicode(false);
+            }
+            );
         }// Especificar las caracteristicas de nuestros datos para que no sean muy genéricos
     }
 }
